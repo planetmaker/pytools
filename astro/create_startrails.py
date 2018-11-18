31794e02e90f3f0c3250a4e81b30a2f54ffb9c35
Ingo von Borstel <planetmaker@openttd.org>
2018-11-18 15:51:06 +0100

    Change: Improve colour further

--- astro/create_startrails.py
+++ astro/create_startrails.py
@@ -11,38 +11,43 @@ import imageio
 import numpy as np
 from PIL import Image
 
+from os import listdir
+from os.path import isfile, join
+
+# Files to use
 cr2_path = "/home/planetmaker/Bilder/2017/2017-03-22/"
 
-first_cr2_filename = "IVB_7552.CR2"
+#first_cr2_filename = "IVB_7327.CR2"
+#last_cr2_filename = "IVB_7337.CR2"
+
+first_cr2_filename = "IVB_7266.CR2"
 last_cr2_filename = "IVB_7554.CR2"
 
-#first_cr2_filename = "IVB_7266.CR2"
-#last_cr2_filename = "IVB_7554.CR2"
-
+# Output filename:
 jpg_filename = "/home/planetmaker/Bilder/test.jpeg"
 
 def process(raw):
     ret = raw.postprocess(
-            output_color=rawpy.ColorSpace.raw, 
-            gamma = (1,1), 
+#            output_color=rawpy.ColorSpace.raw, 
+            output_color=rawpy.ColorSpace.sRGB,
+            gamma = (2.222, 4.5), 
             output_bps = 16,
             use_camera_wb=False,
-            user_wb=[2.0, 1.0, 2.0, 1.0],
+            user_wb=[0.75, 0.5, 1, 0.5],
             no_auto_bright=True,
-            bright=1.0,
-            demosaic_algorithm=rawpy.DemosaicAlgorithm.LINEAR,
+            bright=0.1,
+            user_sat=0,
+            demosaic_algorithm=rawpy.DemosaicAlgorithm.DCB,
             )
 
-    return ret
+    return ret.astype('float32')
 
-from os import listdir
-from os.path import isfile, join
-onlyfiles = sorted([f for f in listdir(cr2_path) if isfile(join(cr2_path, f))])
+allfiles = sorted([f for f in listdir(cr2_path) if isfile(join(cr2_path, f))])
 
-index_first = [i for i, f in enumerate(onlyfiles) if f == first_cr2_filename][0]
-index_last  = [i for i, f in enumerate(onlyfiles) if f == last_cr2_filename][0]
+index_first = [i for i, f in enumerate(allfiles) if f == first_cr2_filename][0]
+index_last  = [i for i, f in enumerate(allfiles) if f == last_cr2_filename][0]
 
-cr2_filenames = onlyfiles[index_first:index_last]
+cr2_filenames = allfiles[index_first:index_last]
 
 with rawpy.imread(cr2_path + cr2_filenames[0]) as raw:
     img = process(raw)
@@ -55,7 +60,17 @@ for file in cr2_filenames[1:]:
     img = np.maximum(current, img)
     current = img
 
-imageio.imsave(jpg_filename, img)
+noise_supp = 5000
+logimg = np.log10(img+noise_supp+1)-np.log10(noise_supp)
+maxlog = np.percentile(logimg, 97)
+logimg = np.clip(logimg * 255 / maxlog, 0, 255).astype('uint8')
+disp2 = Image.fromarray(logimg, 'RGB')
+disp2.show()
 
-disp = Image.fromarray((img / 255).astype('uint8'), 'RGB')
-disp.show()
+maximg = np.percentile(img, 97)
+img = np.clip(img * 255 / maximg, 0, 255).astype('uint8')
+#disp = Image.fromarray(img, 'RGB')
+#disp.show()
+
+imageio.imsave(jpg_filename, logimg)
+
