cb6d7df9d6a25b029eaa4c3258591ba582630e86
planetmaker <planetmaker@openttd.org>
2018-12-06 16:36:05 +0100

    Change/Fix: Image files are differently named. And use a list to retain sorting

--- astro/align_images_to_video.py
+++ astro/align_images_to_video.py
@@ -6,7 +6,7 @@ Created on Wed Dec  5 15:49:58 2018
 @author: ingo
 """
 
-path = "/home/ingo/ownCloud/planetmaker/sternwarte/jochen/Merkurtransit - jpg 09.05.2016/"
+path = "/home/ingo/ownCloud/planetmaker/sternwarte/jochen/Merkurtransit - jpg 09.05.2016/renamed/"
 pattern = "LIGHT*jpg"
 
 import cv2
@@ -17,14 +17,15 @@ import sys
 sys.path.append("../imaging/")
 from detect_circles import detect_circles
 from rotate_image import rotate_image
-#from .imaging import detect_circles
+#from ..imaging import detect_circles
+#from imaging import rotate_image
 
 #def align_images_to_video(path, pattern, output="out.mp4"):
 os.chdir(path)
-files = sorted(glob.glob(pattern), reverse=True)
+files = sorted(glob.glob(pattern))
 
-img_info = dict()
-for file in files:
+img_info = []
+for file in files[350:353]:
     img = cv2.imread(path + file)
     try:
         width, height, channels = img.shape
@@ -37,12 +38,14 @@ for file in files:
     # Make sure that they are all in the same rotation state. That curiously is not the case by default.
     rotate = 0
     if height > width:
-        thumb = rotate_image(thumb)
+        rot = rotate_image(thumb)
         rotate = 90
-        height, width, channels = thumb.shape
+        height, width, channels = rot.shape
+    else:
+        rot = thumb
 
     # Detect the circles and find the best.
-    circles = detect_circles(thumb)
+    circles = detect_circles(rot)
     if circles is None:
         print("No circle found in {}".format(file))
         continue
@@ -57,34 +60,42 @@ for file in files:
                     'y':circle[0][1],
                     'r':circle[0][2],
                     }
-    img_info[file] = {
+    img_info.append({
+            'name':file,
             'width': width,
             'height': height,
             'channels': channels,
             'circles': circles,
             'rotate':rotate,
             'best': best,
-            }
+            })
 
 print(img_info)
 # print(files)
 
 # show the last image with circles superimposed
 cv2.circle(
-        thumb, # image
-        (img_info[file].get('best').get('x'), img_info[file].get('best').get('y')), # point (x,y)
-        img_info[file].get('best').get('r'), # radius
+        rot, # image
+        (img_info[-1].get('best').get('x'), img_info[-1].get('best').get('y')), # point (x,y)
+        img_info[-1].get('best').get('r'), # radius
         (0,255.0), # colour
         2 # linethickness
         )
 cv2.circle(
-        thumb,
-        (img_info[file].get('best').get('x'), img_info[file].get('best').get('y')),
+        rot,
+        (img_info[-1].get('best').get('x'), img_info[-1].get('best').get('y')),
         2,
         (0,0.255),
         3
         )
-cv2.imshow('circles', thumb)
+cv2.imshow('circles', rot)
+
+#fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v')
+#video = cv2.VideoWriter(filename="out.mp4", fourcc, 25, (width, height), True)
+#count = 0
+#
+#for name,data in img_info.items():
+#    print(name, data)
 
 
 #if __name__ == "__main__":
