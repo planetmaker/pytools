733e79fb7af06c7551d2786c2f5045aef7585f98
planetmaker <planetmaker@openttd.org>
2018-12-06 16:34:54 +0100

    Change/Fix: Seems that rotation must not be done around the center of the image...

--- imaging/rotate_image.py
+++ imaging/rotate_image.py
@@ -8,8 +8,12 @@ Created on Thu Dec  6 11:03:16 2018
 
 import cv2
 
-def rotate_image(img, angle=90):
+def rotate_image(img, angle=90, center=None):
     height, width, channels = img.shape
-    M = cv2.getRotationMatrix2D((width/2, height/2), angle, 1)
+    if center == None:
+        # this is strange and maybe a bug in cv2
+        # TODO: this also likely breaks when rotating from wide images to high ones
+        center = (width/2, width/2)
+    M = cv2.getRotationMatrix2D(center, angle, 1)
     ret = cv2.warpAffine(img, M, (height, width))
     return ret
