8bec5d15055d14bba0ebf20c00b255a847b7d27d
planetmaker <planetmaker@openttd.org>
2018-12-06 09:22:06 +0100

    Codechange: Expose the other parameters of the Hough transform also in detect_circles

--- imaging/detect_circles.py
+++ imaging/detect_circles.py
@@ -9,21 +9,23 @@ Created on Tue Dec  4 12:37:34 2018
 import cv2
 import numpy as np
 
-def detect_circles(img, relative_radius_percent=20):
+def detect_circles(img, relative_radius_percent=20, gradient=10, threshold=40, minRadius=None, maxRadius=None):
     try:
         height, width, channels = img.shape
     except AttributeError:
         return None
 
     radius = max([height, width]) / 2
-    radius_min = int(radius * (100-relative_radius_percent)/100)
-    radius_max = int(radius * (100+relative_radius_percent)/100)
+    if minRadius is not None:
+        radius_min = int(radius * (100-relative_radius_percent)/100)
+    if maxRadius is not None:
+        radius_max = int(radius * (100+relative_radius_percent)/100)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, radius,
-                  param1=50,
-                  param2=40,
+                  param1=gradient,
+                  param2=threshold,
                   minRadius=radius_min,
                   maxRadius=radius_max)
 
