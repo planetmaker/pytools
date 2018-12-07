593a0a796efcb627ca818a1ad7fbb0f8956ee3de
planetmaker <planetmaker@openttd.org>
2018-12-07 10:16:29 +0100

    Add: Allow selection of the best fit circle when a reference x,y is passed

--- imaging/detect_circles.py
+++ imaging/detect_circles.py
@@ -9,7 +9,7 @@ Created on Tue Dec  4 12:37:34 2018
 import cv2
 import numpy as np
 
-def detect_circles(img, relative_radius_percent=20, gradient=10, threshold=40, minRadius=None, maxRadius=None):
+def detect_circles(img, relative_radius_percent=20, gradient=10, threshold=40, minRadius=None, maxRadius=None, similar=None):
     try:
         height, width, channels = img.shape
     except AttributeError:
@@ -30,7 +30,24 @@ def detect_circles(img, relative_radius_percent=20, gradient=10, threshold=40, m
                   maxRadius=maxRadius,
                   )
 
-    return circles
+    if similar is None:
+        return circles
+    else:
+        best = {'i':None, 'dist':0, 'x':None, 'y':None, 'r':None}
+        if circles is None:
+            return best, None
+        for n,circle in enumerate(circles):
+            dist = min([similar.get('bestx')-circle[0][0],similar.get('besty')-circle[0][1]])
+            if dist > best.get('dist'):
+                best = {
+                        'i':n,
+                        'dist':dist,
+                        'x':circle[0][0],
+                        'y':circle[0][1],
+                        'r':circle[0][2],
+                        }
+        return best, circles
+
 
 if __name__ == "__main__":
     img = cv2.imread('/home/ingo/pytools/examples/sun_example2.jpg',cv2.IMREAD_COLOR)
