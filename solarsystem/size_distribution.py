a54a8f0a72d7cffc491a95b060402bd28b5ee658
planetmaker <planetmaker@planetmaker.de>
2023-12-29 15:14:01 +0100

    Add: size distribution of solar system moons

--- solarsystem/size_distribution.py
+++ solarsystem/size_distribution.py
@@ -32,25 +32,63 @@ def parse_radius(col_df) -> float():
         # print(col)
     return col
         
+def filter_moons(df):
+    dff = df[df['Type'].str.startswith('moon of')]
+    print(dff)
+    return dff
 
 radii = []
 skiprows = 2
-for i in range(5):
+for i in range(7):
     if i > 0:
         skiprows = 1
     df = pd.read_excel(filename, engine="odf", skiprows=skiprows, sheet_name=i, decimal='.')
     # print(df)
-    radii.extend(parse_radius(df['radius_km']))
+    radius = parse_radius(df['radius_km'])
+    # Bodies smaller than 1km are given in m
+    if i == 6:
+        for j,r in enumerate(radius):
+            radius[j] = r/1000
+    radii.extend(radius)
     # print(radii)
     
 count = [x for x in range(len(radii))]
 
 fig,ax = plt.subplots()
 ax.scatter(radii,count, marker='+', linewidth=1.0)
-ax.set(xlim=(10,100000), ylim=(1,1000))
+ax.set(xlim=(0.01,100000), ylim=(1,1000))
 plt.yscale('log')
 plt.xscale('log')
 plt.title('size distribution of solar system bodies')
+plt.xlabel('size [km]')
+plt.ylabel('count bodies larger than size')
+plt.show()
+
+#only moons
+moons = []
+skiprows = 2
+for i in range(7):
+    if i > 0:
+        skiprows = 1
+    dff = pd.read_excel(filename, engine="odf", skiprows=skiprows, sheet_name=i, decimal='.')
+    dff = filter_moons(dff)    
+    # print(df)
+    radius = parse_radius(dff['radius_km'])
+    # Bodies smaller than 1km are given in m
+    if i == 6:
+        for j,r in enumerate(radius):
+            radius[j] = r/1000
+    moons.extend(radius)
+    # print(radii)
+    
+countm = [x for x in range(len(moons))]
+
+fig,ax = plt.subplots()
+ax.scatter(moons,countm, marker='+', linewidth=1.0)
+ax.set(xlim=(0.01,100000), ylim=(1,1000))
+plt.yscale('log')
+plt.xscale('log')
+plt.title('size distribution of solar system moons')
 plt.xlabel('size [km]')
 plt.ylabel('count bodies larger than size')
 plt.show()
